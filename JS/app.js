b86082a83bb7c28f8c22dac9a99060643be292f7
Maram Hani Mohammad <maramhanisbaih@gmail.com>
2021-02-23 16:00:18 +0200

    added js

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+var age= 25;
+var specialty= "software development";
+
+
+if (age<18){
+    console.log("You can learn coding in your age, Keep learning");
+}
+
+if (age>-25){
+    alert("Are you ready to Learn coding");
+}
+
+
+var ageUserInput=prompt("How old are you?");
+var specialtyUserInput=prompt("What are you studing?");
+
+if(specialty= "software development")
+{
+console.log(" Keep going and stay up to date");
+}
+
+else {
+    console.log("Evreyone can learn coding");
+}
+
+var txt;
+var r = confirm("Enjoy Your journey learning!");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+
